43a69eaa41d1d90ee160a206642e8727d8630ea0
Md. Shaheduzzaman <mdshaheduzzaman67@gmail.com>
2020-05-13 17:44:06 +0600

    Order page Created

--- lib/widget/DrinkList.dart
+++ lib/widget/DrinkList.dart
@@ -6,7 +6,7 @@ import 'package:shopapp/widget/DrinkCards.dart';
 
 class DrinkList extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext scaffoldContext) {
     return ScopedModelDescendant<DrinkListModel>(
       builder: (context, _, model){
         return Expanded(
@@ -14,8 +14,111 @@ class DrinkList extends StatelessWidget {
             padding: EdgeInsets.all(6.0),
             crossAxisCount: 2,
             children: model.drinkType.map((drinkType){
-              return DrinkCards(
-                drinkType: drinkType,
+              return GestureDetector(
+                onTap: (){
+                  showDialog(
+                      context: scaffoldContext,
+                    builder:  (BuildContext dialogContext){
+                        return AlertDialog (
+                          title: Text(
+                            "Order"
+                          ),
+                          content: DecoratedBox(
+                            decoration: BoxDecoration(
+                              color: Colors.yellowAccent,
+                            ),
+                            child: Row (
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Image.asset(
+                                  drinkType.image,
+                                  height: 100.0,
+                                  width: 100.0,
+                                  fit: BoxFit.cover,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        drinkType.title,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.0,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Price :\$${drinkType.price}"
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text(
+                                "Send",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Colors.green,
+                                ),
+                              ),
+                              onPressed: (){
+                                Navigator.pop(dialogContext);
+                                final snackBar = SnackBar (
+                                  backgroundColor: Colors.white,
+                                  duration: Duration(
+                                    seconds: 4,
+                                  ),
+                                  content: Text(
+                                    "${drinkType.title} order confirmed !",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20.0,
+                                    ),
+                                  ),
+                                );
+                                Scaffold.of(scaffoldContext).showSnackBar(snackBar);
+                              },
+                            ),
+                            FlatButton(
+                              child: Text(
+                                "Cancel",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Colors.red,
+                                ),
+                              ),
+                              onPressed: (){
+                                Navigator.pop(dialogContext);
+                                final snackbar = SnackBar(
+                                  backgroundColor: Colors.white,
+                                  duration: Duration(
+                                    seconds: 4,
+                                  ),
+                                  content: Text(
+                                    "${drinkType.title} order Cencled !",
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                );
+                                Scaffold.of(scaffoldContext).showSnackBar(snackbar);
+                              },
+                            ),
+                          ],
+                        );
+                    }
+                  );
+                },
+                child: DrinkCards(
+                  drinkType: drinkType,
+                ),
               );
             }).toList() ,
           ),
