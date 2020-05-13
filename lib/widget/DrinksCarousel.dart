477ceb1ab49321809ddf8c0fcd255287c6f32b91
Md. Shaheduzzaman <mdshaheduzzaman67@gmail.com>
2020-05-13 14:40:14 +0600

    Slider Animation Created

--- lib/widget/DrinksCarousel.dart
+++ lib/widget/DrinksCarousel.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:shopapp/widget/DrinkCards.dart';
 
-class DrinksCarousel extends StatelessWidget {
+class DrinksCarousel extends StatefulWidget {
+  @override
+  _DrinksCarouselState createState() => _DrinksCarouselState();
+}
+
+class _DrinksCarouselState extends State<DrinksCarousel> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: cofeeType.length, vsync: this);
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _tabController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -14,8 +34,48 @@ class DrinksCarousel extends StatelessWidget {
             color: Colors.black26,
           )
         ),
-        child: DrinkCards(
-          drinkType: mainType[0],
+        child: Stack(
+          children: <Widget>[
+            TabBarView(
+              controller: _tabController,
+              children: mainType.map((drinkType){
+                return DrinkCards(
+                  drinkType: drinkType,
+                );
+              }).toList(),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TabPageSelector(
+                  controller: _tabController,
+                  color: Colors.white,
+                  indicatorSize: 18.0,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_left,
+                  color: Colors.white,
+                  size: 40.0,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_right,
+                  color: Colors.white,
+                  size: 40.0,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
